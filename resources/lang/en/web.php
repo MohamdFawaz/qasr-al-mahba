e1185c3fc96631e4e5dfe09f942643d32f308d5b
MohamdFawaz <mohamdfawaz93@gmail.com>
2021-08-29 10:21:29 -0400

    Add lang

---
+++ resources/lang/en/web.php
@@ -0,0 +1,247 @@
+<?php
+
+return array (
+  'header' => 
+  array (
+    'home' => 'Home',
+    'services' => 'Services',
+    'product' => 'Products',
+    'contact' => 'Contact Us',
+    'about' => 'About Us',
+    'get_fair_quote' => 'Get A Quote',
+    'mining_license' => 'Mining License',
+    'mining_process' => 'Mining Process',
+  ),
+  'home' => 
+  array (
+    'contact_us' => 
+    array (
+      'full_name' => 'Name',
+      'email' => 'example@mail.com',
+      'phone_number' => 'Mobile Number',
+      'subject' => 'Subject',
+      'short_text' => 'Description',
+      'submit' => 'submit',
+      'address_1_title' => 'HQ',
+      'address_1_desc' => '310, Binary Tower, Business Bay, Dubai, UAE',
+      'address_2_title' => 'Branch',
+      'email_us_title' => 'Email Address',
+      'email_us_content' => 'info@qasralmahba.com',
+      'call_us_title' => 'Telephone no.',
+      'call_us_dec' => '045786888',
+      'project_scope' => 'Subject',
+      'contact_title' => 'Have an Inquiry ?',
+      'contact_desc' => 'Fill out This Form and we will contact you back as soon as we can.',
+    ),
+    'mining_license_title' => 'mining',
+    'mining_license_content' => 'Invest In Mining In Mozambique',
+    'read_more' => 'More',
+    'game_skin_title' => 'Resource Mining',
+    'game_skin_content' => 'The Mining Process of Each Resource',
+    'new_product_title' => 'New Product',
+    'new_product_content' => 'Find our newest products',
+    'who_we_are_title' => 'About Us',
+    'who_we_are_content' => 'We offer many products and services such as mining licenses in many parts in Mozambique',
+    'founders_content' => 'Helping our partners reach their goals and setting up their business in mining in Mozambique by helping them turn their investment into profit.',
+    'founders_title' => 'Founder',
+    'our_trusted' => 'Trusted',
+    'partners' => 'Partners',
+    'contact_us_title' => 'Where You Can Find Us',
+    'contact_us_content' => 'Need Help ? Type Your Inquiry.',
+    'footer' => 
+    array (
+      'out_services_title' => 'Services',
+      'mining_license' => 'Mining License',
+      'game_skin' => 'Resource Mining',
+      'new_product' => 'New Product',
+      'contact_us' => 'Contact Us',
+      'first_address' => '310, Binary tower, Business Bay, Dubai, UAE',
+      'mobile_number' => '045786888',
+      'email' => 'info@qasralmahba.com',
+      'about_title' => 'About Us',
+      'about_link_title_1' => 'Profile',
+      'about_link_title_2' => 'Expertise',
+      'about_link_title_3' => 'Partnership',
+      'second_address' => 'Second Address Soon',
+      'mining_process' => 'Mining Process',
+    ),
+    'banner' => 
+    array (
+      'contact_us' => 'Contact Us',
+    ),
+    'invest_in_mozambique_title' => 'Invest In Mozambique',
+    'invest_in_mozambique_description' => 'Mining in Mozambique plays a significant role in the world’s production of coal, gold, graphite and ilmenite. Currently, most of Mozambique’s mining and mineral processing operations are privately owned, including cement plants, aluminum smelters and gas processing plants.',
+    'mining_process_title' => 'Mining Process',
+    'mining_process_content' => 'Discover More About Mining',
+    'articles' => 'Articles',
+  ),
+  'title' => 
+  array (
+    'mining_process' => 'Mining Process',
+    'animal_skin' => 'Resource Mining',
+    'qasr_al_mahba' => 'Qasr Al Mahba',
+    'mining_license' => 'Mining License',
+    'home' => 'home',
+    'welcome_to_qasr_al_mahba' => 'Welcome To Qasr Al Mahba',
+    'about' => 'About',
+    'contact' => 'Contact',
+  ),
+  'footer' => 
+  array (
+    'about_us_content' => 'Your Trust Is Our Treasure',
+    'facebook_link' => 'https://www.facebook.com/Qasralmahba/',
+    'twitter_link' => 'https://twitter.com/qasralmahba',
+    'linkedin_link' => 'https://www.linkedin.com/in/qasr-al-mahba-355943219/',
+    'youtube_link' => 'https://www.youtube.com/channel/UC9v4n-qWIKS_lqRbR0eQ0MA',
+    'instagram_link' => 'https://www.instagram.com/qasralmahba/',
+  ),
+  'breadcrumb' => 
+  array (
+    'home' => 'Home',
+    'mining_license' => 'Services',
+    'animal_skin' => 'Products',
+    'show_animal_skin' => 'Resource Mining',
+    'about' => 'About Us',
+    'contact' => 'Contact Us',
+    'mining_process' => 'Mining Process',
+    'show_mining_process' => 'Mining Resource Process',
+  ),
+  'page' => 
+  array (
+    'title' => 
+    array (
+      'mining_license' => 'Mining Licenses',
+      'animal_skin' => 'Resource Mining',
+      'mining_process' => 'Mining Process',
+    ),
+    'mining' => 
+    array (
+      'guaranteed_return_of_investment_title' => 'Guaranteed Return Of Investment',
+      'guaranteed_return_of_investment_desc' => 'Get ahead with Africa Mining IQ and take advantage of its ever-growing database of key contact details, project status updates and estimated values of over 62 mining projects in Mozambique.
+Our database of mining in Mozambique covers all the dominant minerals of the country: Coal, Fluorspar, Gold, Graphite, Ilmenite, Industrial, Iron Ore, Nickel, PGM – Platinum Group Metals, Phosphate, Rare Earth Minerals/Metals, Tantalum/Tantalite, Undefined, Uranium and Zinc.',
+      'set_costs_title' => 'costs',
+      'set_costs_desc' => 'costs',
+      'calculate_title' => 'calculate_title',
+      'calculate_desc' => 'calculate_desc',
+      'contact_us_title' => 'Contact Us',
+      'contact_us_desc' => '045786888',
+      'title' => 
+      array (
+        'mining' => 'Mining',
+        'resources' => 'Resources',
+      ),
+      'resource_1_desc' => 'Mozambique has the highest untapped coal deposits. They have high quality coking coal and thermal coal. The coking coal can be used to make steel and any other metals that are used in building, tools, surgical tools, and implants.',
+      'resource_2_desc' => 'Mozambique has high-quality iron ore which is used in making steel and some of the powdered iron is used for different purposes like magnets, auto parts and the radioactive iron (iron 59) is used for medicine as a tracer element.',
+      'resource_3_desc' => 'Moma which is 160 Km away from the city Nampula in Mozambique has the bigest titanium mineral deposits where ilmenite, rutile and zircon are found. They are the major source to produce titanium oxide pigments.',
+      'resource_4_desc' => 'In the four following provinces: Gaza, Manica, Tete, and Niassa diamond reserve can be found. They have commercial grade diamond which makes up a strong percentage of the diamond market. The most common use of diamond nowadays is as a gemstone. However, diamond can also be used as an industrial abrasive for cutting hard materials.',
+      'available_license_title' => 'Start Your Business In Mining NOW',
+      'codes_title' => 'Find the available mining license codes here',
+      'select_code_placeholder' => 'Press to Copy the license number to clipboard',
+      'use_the_code_for_information_and_location_preview' => 'Click on the "HERE" button to redirect to the Map Website',
+      'here' => 'HERE',
+      'code_content' => 'After clicking the code it will be copied on your clipboard , go to the website below to check the license details',
+    ),
+    'animal_skin' => 
+    array (
+      'natural_title' => 'Precious',
+      'natural_desc' => 'Get Your Hands On Precious Resources',
+      'high_quality_title' => 'Profitable',
+      'high_quality_desc' => 'Turn Your Investment Into Profit',
+      'contact_us_title' => 'contact us',
+      'contact_us_desc' => '045786888',
+      'content_title' => 'Resource Mining',
+      'content_description' => 'Resource Mining Description',
+      'categories' => 'Mining Process',
+      'realistic_image_for' => 'Mining Process Of',
+    ),
+    'show_animal_skin' => 
+    array (
+      'product' => 
+      array (
+        'delivery' => 'Product',
+        'free_delivery' => 'Delivery',
+      ),
+    ),
+    'about' => 
+    array (
+      'who' => 'Qasr',
+      'we' => 'Al',
+      'are' => 'Mahba',
+      'integrity_title' => 'Mining Licenses',
+      'integrity_content' => 'Find more information on our blog',
+      'automation_title' => 'Mining Taxes',
+      'automation_content' => 'Find more information on our blog',
+      'tradition_title' => 'Resource Production',
+      'tradition_content' => 'Find more information on our blog',
+      'safety_title' => 'About Mozambique',
+      'safety_content' => 'Find more information on our blog',
+      'why_choose_us_title' => 'Why Choose Us',
+      'out_expertise_title' => 'Expertise',
+      'out_expertise_content' => 'content',
+      'out_expertise_title_list_item_1' => 'Expertise 1',
+      'out_expertise_title_list_item_2' => 'Expertise 2',
+      'out_expertise_title_list_item_3' => 'Expertise 3',
+      'integrity_link' => 'https://qasralmahba.com/',
+      'automation_link' => 'https://qasralmahba.com/',
+      'tradition_link' => 'https://qasralmahba.com/',
+      'safety_link' => 'https://qasralmahba.com/',
+    ),
+    'contact' => 
+    array (
+      'let_get_in_touch_title' => 'Get In Touch',
+      'get_in_touch_title_list_item_1' => 'Call Us On Our Number : 045786888',
+      'get_in_touch_title_list_item_2' => 'Fill Out the Form Below And We Will Call You Back As Soon As we can',
+      'get_in_touch_title_list_item_3' => 'Send An Email To : info@qasralmahba.com With Your Inquiry',
+      'get_in_touch_title_list_item_4' => 'Ask Us Through Our Social Media Platforms',
+      'follow_us' => 'Social Media',
+      'monday_name' => 'Monday',
+      'tuesday_name' => 'Tuesday',
+      'wednesday_name' => 'Wednesday',
+      'sunday_name' => 'Sunday',
+      'thursday_name' => 'Thursday',
+      'friday_name' => 'Friday',
+      'saturday_name' => 'Saturday',
+      'sunday_time_range' => '9:00AM - 8:00PM',
+      'monday_time_range' => '9:00AM - 8:00PM',
+      'tuesday_time_range' => '9:00AM - 8:00PM',
+      'wednesday_time_range' => '9:00AM - 8:00PM',
+      'thursday_time_range' => '9:00AM - 8:00PM',
+      'friday_time_range' => 'Day Off',
+      'saturday_time_range' => '9:00AM - 8:00PM',
+    ),
+    'mining_process' => 
+    array (
+      'contact_us_title' => 'Contact Us',
+      'contact_us_desc' => 'Contact Us Description',
+      'content_title' => 'Mining Process Content',
+      'content_description' => 'Mining Process Description',
+      'categories' => 'Available Resources',
+      'realistic_image_for' => 'The Mining Process Of',
+    ),
+  ),
+  'show_animal_skin' => 
+  array (
+    'famous_products' => 'Famous Products',
+  ),
+  'pages' => 
+  array (
+    'about' => 
+    array (
+      'about_us_title' => 'About Us',
+      'who_we_are_section' => 'Who We Are Section',
+      'qasr_al_mahba_vision' => 'Vision',
+      'qasr_al_mahba_vision_content' => 'Vision Content',
+      'why_choose_us_content' => 'We are the Best in our Domain',
+      'contact_us_title' => 'Call Us',
+      'qasr_al_mahba_location_content' => 'Find Us At Office 310, The Binary Tower, Business Bay, Dubai, UAE',
+    ),
+    'contact' => 
+    array (
+      'qasr_al_mahba_location_title' => 'Location',
+    ),
+  ),
+  'show_mining_process' => 
+  array (
+    'famous_products' => 'Famous Products',
+  ),
+);
