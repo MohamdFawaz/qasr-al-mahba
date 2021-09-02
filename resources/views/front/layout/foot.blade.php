2c1f1361879cefaf1da39ac844f6bae2b340c264
mohamdfawaz <mohamdfawaz93@gmail.com>
2021-09-02 10:29:30 +0200

    Fixes and improvement

--- resources/views/front/layout/foot.blade.php
+++ resources/views/front/layout/foot.blade.php
@@ -2,7 +2,6 @@
 <script src="{{asset('front/js/jquery-3.5.1.min.js')}}"></script>
 <script src="{{asset('front/js/bootstrap.min.js')}}"></script>
 <script src="{{asset('front/js/slick.min.js')}}"></script>
-<script src="{{asset('front/js/wow.min.js')}}"></script>
 <script src="{{asset('front/js/leaflet.min.js')}}"></script>
 <script src="{{asset('front/js/appear.js')}}"></script>
 <!-- Custom script -->
